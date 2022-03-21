752b6fb228537da41df4cd3b379b83ae20a4609b
Ken Haberer <keha@odoo.com>
2022-03-21 19:13:15 +0000

    Trying again

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    
+    'summary': """Academy app to manage Training""",
+    
+    'description': """
+        Academy Module to manage Training:
+        - Course
+        - Sessions
+        - Attendees
+    """,
+    
+    'author': 'Odoo',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [        
+    
+    ],
+    
+    'demo': [
+        
+    ],
+}
